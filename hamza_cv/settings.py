4d1c6893b979e76ad42b233fa71e9261f4ba2f1e
Hamza Ahmad <hamzaahamd248@gmail.com>
2026-02-08 18:01:54 +0500

    abc

--- hamza_cv/settings.py
+++ hamza_cv/settings.py
@@ -161,9 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Tell Django where to find static files from apps
 STATICFILES_DIRS = [
--- hamza_cv/wsgi.py
+++ hamza_cv/wsgi.py
@@ -8,21 +8,9 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hamza_cv.settings')
 
-try:
-	app = get_wsgi_application()
-	
-	# Wrap with WhiteNoise for static files
-	BASE_DIR = Path(__file__).resolve().parent.parent
-	app = WhiteNoise(app, root=str(BASE_DIR / 'staticfiles'))
-except Exception as e:
-	import sys, traceback
-	tb = traceback.format_exc()
-	print("WSGI startup error:\n" + tb, file=sys.stderr)
-	raise
+app = get_wsgi_application()
